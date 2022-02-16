c261edb0d6999292cbf203d4b0daeb6ec14daf70
MartenYeager <marten.ter.meer@gmail.com>
2022-02-16 12:42:25 +0100

    Added "Functional" Log in page

---
+++ WebDevStripboeken/WebDevStripboeken/Pages/LogIn.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebDevStripboeken.Pages;
+
+public class LogIn : PageModel
+{
+    [BindProperty]
+    public string User { get; set; }
+    public string WW { get; set; }
+    public void OnGet()
+    {
+        
+    }
+
+    public void OnPostSignup([FromForm] string newUser, [FromForm] string newWW)
+    {
+        User = newUser;
+        WW = newWW;
+    }
+    public void OnPostLogin([FromForm] string User, [FromForm] string WW)
+    {
+        this.User = User;
+        this.WW = WW;
+    }
+}
