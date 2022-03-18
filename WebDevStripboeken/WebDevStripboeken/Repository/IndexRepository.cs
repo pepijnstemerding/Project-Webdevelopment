75733d2374899310935a5e4c02035c48456dc42a
Dolfijn004 <jasonachtien18@gmail.com>
2022-03-18 11:09:54 +0100

    fixed db connection (I hope)

---
+++ WebDevStripboeken/WebDevStripboeken/Repository/IndexRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Dapper;
+using WebDevStripboeken.Models;
+
+namespace WebDevStripboeken.Repository;
+
+public class IndexRepository : DBConnection
+{
+    public int iets()
+    {
+
+        using var connection = Connect();
+        
+        return 6;
+    }
+}
