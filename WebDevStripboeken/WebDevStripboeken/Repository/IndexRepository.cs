63c8dad1399c3776ef8f98e13c265c4cf43d5c15
Honor <71923017+Honor-js@users.noreply.github.com>
2022-03-26 22:09:37 +0100

    NAV BAR IS RESPONSIVE

--- WebDevStripboeken/WebDevStripboeken/Repository/IndexRepository.cs
+++ WebDevStripboeken/WebDevStripboeken/Repository/IndexRepository.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using Dapper;
+﻿using Dapper;
+using System;
 using WebDevStripboeken.Models;
 
 namespace WebDevStripboeken.Repository;
 
 public class IndexRepository : DBConnection
 {
-    public int iets()
+    public static List<myStripboek> GetAll(int min)
     {
-
+        var parameters = new {Min = min, Max = min + 5};
         using var connection = Connect();
-        
-        return 6;
+
+        IEnumerable<myStripboek> all3 = connection.Query<myStripboek>(
+            @"SELECT *
+                FROM (website.stripboek)
+                WHERE Boek_id >= @Min AND Boek_id < @Max;", parameters);
+        return all3.ToList();
     }
 }
