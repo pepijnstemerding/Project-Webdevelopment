32b81305eda714d9d33956b261ab6372197c86fc
MartenYeager <marten.ter.meer@gmail.com>
2022-03-28 15:36:16 +0200

    current iteration stripboekrepository

--- WebDevStripboeken/WebDevStripboeken/Repository/SearchRepository.cs
+++ WebDevStripboeken/WebDevStripboeken/Repository/SearchRepository.cs
@@ -7,15 +7,17 @@ public class SearchRepository : DBConnection
 {
     public static List<myStripboek> SearchSubmit(string a, string b)
     {
+        //controle a
+        
         //string c = string.Format("{0}" + b  +"{0}", '%');
         //Console.WriteLine(c);
         var parameters = new {Query = b};
         using var connection = Connect();
 
         IEnumerable<myStripboek> results = connection.Query<myStripboek>(
-            @"SELECT *
+            $@"SELECT *
                 FROM (website.stripboek)
-                WHERE " + a +" LIKE CONCAT('%', @Query, '%');", parameters);
+                WHERE {a} LIKE CONCAT('%', @Query, '%');", parameters);
         // ----> param voor category pruimt ie niet?? <----
         return results.ToList();
     }
--- WebDevStripboeken/WebDevStripboeken/Repository/StripboekRepository.cs
+++ WebDevStripboeken/WebDevStripboeken/Repository/StripboekRepository.cs
@@ -9,12 +9,23 @@ public class StripboekRepository : DBConnection
 {
     public static myStripboek GetOne(int x)
     {
+        //dunno of ik de joins netjes heb geimplementeerd
         var parameters = new {Boekid = x};
         using var connection = Connect();
         myStripboek one = connection.QuerySingle<myStripboek>(
-            @"SELECT *
-                FROM (website.stripboek), (website.auteur),(website.tekenaar)
-                WHERE Boek_id = @Boekid LIMIT 1;", parameters);
+            @"SELECT (stripboek.Boek_id), (stripboek.Reeks), (stripboek.Titel), (stripboek.ISBN), (stripboek.Jaar_v_Uitgave), (auteur.Naam_Autheur), (tekenaar.Naam_Tekenaar), (stripboek.Uitgever), (stripboek.Afbeelding_urls), (stripboek.Waarde_schatting)
+                FROM (website.stripboek)
+                LEFT OUTER JOIN (website.geschreven_door) 
+                ON (website.stripboek.Boek_id) = (website.geschreven_door.Boek_id)
+                LEFT OUTER JOIN (website.getekend_door)
+                ON (website.stripboek.Boek_id) = (website.getekend_door.Boek_id)
+                JOIN (website.auteur)
+                ON (website.geschreven_door.Auteur_id) = (website.auteur.Auteur_id)
+                JOIN (website.tekenaar)
+                ON (website.getekend_door.Tekenaar_id) = (website.tekenaar.Tekenaar_id)
+                WHERE (website.stripboek.Boek_id) = @Boekid LIMIT 1;", parameters);
         return one;
     }
+    //dont work, QuerySingle geeft een enkele rij terug. Dit werkt dus niet met de lijsten myTekenaars en myAuteurs
+    //mogelijke oplossing zou zijn om lijsten appart op te halen en dan in one te zetten.
 }
