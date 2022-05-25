e7e03d8abbac2a84b819333daa2bc08ea2936f1d
Pepijn Stemerding <pepijnding@hotmail.nl>
2022-05-25 11:17:00 +0200

    Fix

--- WebDevStripboeken/WebDevStripboeken/SQL Queries/CreateQuerys.sql
+++ WebDevStripboeken/WebDevStripboeken/SQL Queries/CreateQuerys.sql
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `Zit_in` (
 `Boek_id`       INT,
 `Collectie_id`  INT NOT NULL,
 `Gebruiker_id`  INT NOT NULL, 
-PRIMARY KEY (`Collectie_id`, `Gebruiker_id`),
+PRIMARY KEY (`Boek_id`, `Collectie_id`, `Gebruiker_id`),
 FOREIGN KEY (`Boek_id`) REFERENCES `Stripboek`(`Boek_id`) ON DELETE CASCADE ,
 FOREIGN KEY (`Collectie_id`) REFERENCES `Collectie`(`Collectie_id`),
 FOREIGN KEY (`Gebruiker_id`) REFERENCES `Gebruiker`(`Gebruiker_id`) ON DELETE CASCADE);
